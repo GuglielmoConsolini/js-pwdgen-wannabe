1cec01e23aea357d1b38d89acb1700d9d2acce55
GuglielmoConsolini <consoliniguglielmo@gmail.com>
2024-04-16 16:33:18 +0200

    Generatore password

---
+++ js/main.js
@@ -0,0 +1,13 @@
+alert("Imposta una password super sicura LOL");
+
+const firstName = prompt ( "Il tuo Nome" );
+
+const secondName = prompt ("il tuo secondo Nome");
+
+const color = prompt ("Colore preferito");
+
+let finePassword = "#24";
+
+let passWord = firstName + secondName + color + finePassword ;
+
+alert(passWord);
